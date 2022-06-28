fa14dfd55825f84b6b56a17a495e1137c129d594
dazod <viddadev@gmail.com>
2022-06-29 00:43:57 +0200

    first commit

---
+++ my-app/src/components/chatting/Chatting.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./chatting.css";
+
+function Chatting({ conversation, currentUser }) {
+  const [user, setUser] = useState(null);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  useEffect(() => {
+    const friendId = conversation.members.find((m) => m !== currentUser._id); //m => m oba usera iz jedan razgovor
+
+    const getUser = async () => {
+      try {
+        const res = await axios("/users?userId=" + friendId);
+        setUser(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getUser();
+  }, [currentUser, conversation]);
+
+  return (
+    <div className="chatting">
+      <img
+        className="chattingImg"
+        src={
+          user?.profilePicture
+            ? PF + user.profilePicture
+            : PF + "person/noAvatar.png"
+        }
+        alt=""
+      />
+      <span className="chattingName">{user?.username}</span>
+    </div>
+  );
+}
+export default Chatting;
